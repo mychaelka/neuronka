d8d813cbd628461ad4a7dad10323742b003ead1b
Michaela <mychaela.k98@gmail.com>
2024-09-30 11:09:40 +0200

    Implement activation functions.

--- src/math.cpp
+++ src/math.cpp
@@ -1,17 +1,46 @@
 #include "math.h"
 #include <vector>
 #include <stdexcept>
+#include <iostream>
+#include <cmath>
 
-int dot_product(std::vector<int> u, std::vector<int> v) {
-    int result = 0;
-
+// Linear algebra
+float dot_product(std::vector<float> u, std::vector<float> v) {
+    
     if (u.size() != v.size()) {
-        throw std::length_error("Vectors u and v are not of the same length.");
+        std::cerr << "Vectors u and v are not of the same length." << std::endl;
     }
+
+    float result = 0.0;
 
     for (size_t i = 0; i < u.size(); i++) {
         result += u[i] * v[i];
     }
 
     return result;
+}
+
+// Activation functions
+float sigmoid(float x) {
+    return 1.0f / (1.0f + std::exp(-x));
+}
+
+float sigmoid_prime(float x) {
+    return sigmoid(x) * (1.0f - sigmoid(x));
+}
+
+float relu(float x) {
+    return std::max(0.0f, x);
+}
+
+float relu_prime(float x) {
+    return x > 0.0f ? 1.0f : 0.0f;
+}
+
+float tanh(float x) {
+    return (std::exp(x) - std::exp(-x)) / (std::exp(x) + std::exp(-x));
+}
+
+float tanh_prime(float x) {
+    return tanh(x) * tanh(x);
 }
--- src/math.h
+++ src/math.h
@@ -1,3 +1,17 @@
 #include <vector>
 
-int dot_product(std::vector<int> u, std::vector<int> v);
+// Linear algebra
+float dot_product(std::vector<float> u, std::vector<float> v);
+
+// Activation functions
+float sigmoid(float x);
+
+float sigmoid_prime(float x);
+
+float relu(float x);
+
+float relu_prime(float x);
+
+float tanh(float x);
+
+float tanh_prime(float x);
