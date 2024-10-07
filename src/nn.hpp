ae781e1f1769e50bc110e8e83e79b0afaf968054
Michaela <mychaela.k98@gmail.com>
2024-10-07 15:40:10 +0200

    Add initializers

--- src/nn.hpp
+++ src/nn.hpp
@@ -11,8 +11,6 @@
 namespace nn {
 
     class Neuron {
-        // constructor should initialize weights (they can be computed by the initWeights method?) and take number of weights as an argument
-        // the number of weights = number of neurons in the previous layer
     private:
         Vector _weights;
         size_t _n_weights;
@@ -24,16 +22,28 @@ namespace nn {
             init_weights();
         }
 
-        void activate(const std::vector<float> &inputs) {
-           // this->output = nn::dot_product(weights, inputs) + bias;
+        size_t n_weights() const { return _n_weights; }
+        const Vector& get_weights() const { return _weights; }
+        void set_weights(std::vector<float>& new_weights) { 
+            _weights.set_weights(new_weights);
+        }
+
+        float output() const { return _output; }
+
+        void activate(const nn::Vector& inputs) {
+            _output = _weights.dot_product(inputs) + _bias;
         }
 
         void print_weights() {
+
+            std::cout << "Weights: ";
+
             for (size_t i = 0; i < _n_weights; ++i) {
                 std::cout << _weights[i] << ", ";
             }
 
             std::cout << std::endl;
+            std::cout << "Bias: " << _bias << std::endl;
         }
 
         void init_weights() {
@@ -41,33 +51,42 @@ namespace nn {
             std::mt19937 generator(rd());
 
             std::normal_distribution<float> distribution(0.0f, std::sqrt(2.0f / _n_weights));
+            std::uniform_real_distribution<float> bias_distribution(-0.1f, 0.1f);
 
             for (size_t i = 0; i < _n_weights; ++i) {
                 _weights[i] = distribution(generator);
             }
+
+            _bias = bias_distribution(generator);
         }
     };
 
 
     class Layer {
     private:
-        std::vector<Neuron> neurons;
+        std::vector<Neuron> _neurons;
     
     public:
+        Layer(size_t n_neurons, size_t n_weights_per_neuron) {
+            for (size_t i = 0; i < n_neurons; ++i) {
+                _neurons.emplace_back(n_weights_per_neuron);
+            }
+        }
         std::vector<float> get_outputs() const;
-        std::vector<Neuron> &get_neurons() const;
+        const std::vector<Neuron> &get_neurons() const { return _neurons; }
     };
 
 
     class MLP {
     private:
-        std::vector<Layer> layers;
+        std::vector<Layer> _layers;
     
     public:
+        MLP() {} // TODO: initializer list
         void feed_forward() {
-            for (int i = 1; i < this->layers.size(); ++i) { // for each layer
-                for (Neuron neuron : this->layers[i].get_neurons()) { //
-                    neuron.activate(this->layers[i-1].get_outputs());
+            for (int i = 1; i < this->_layers.size(); ++i) { // for each layer
+                for (Neuron neuron : this->_layers[i].get_neurons()) { //
+                    neuron.activate(this->_layers[i-1].get_outputs());
                 }
             }
         }
