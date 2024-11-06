8aefcc34159eb72c8ab6d2514238e781c6b47c78
Michaela <mychaela.k98@gmail.com>
2024-11-06 12:58:08 +0100

    backprop tests

--- src/tests.hpp
+++ src/tests.hpp
@@ -50,86 +50,81 @@ void tests() {
     std::cout << "+= modifies given vector: " << std::endl;
     vec1.print_readable();
 
-    // Matrix
-    nn::Matrix matrix1 = nn::Matrix(4, 3);
-    nn::Matrix matrix2 = nn::Matrix(3, 2);
-    nn::Matrix matrix3 = nn::Matrix(5, 2);
-
-    matrix1.set(0, 0, 1);
-    matrix1.set(0, 2, 3);
-    matrix1.set(1, 1, 6);
-    matrix1.set(0, 0, 1);
-    matrix1.set(0, 3, 4);
-    matrix1.set(2, 0, 5);
-    matrix1.set(2, 0, 2);
-    matrix1.set(3, 1, 2);
-    matrix1.set(3, 2, 7);
-    matrix1.print_readable();
-
-    matrix2.set(0, 1, 1);
-    matrix2.set(1, 1, 3);
-    matrix2.set(2, 1, 6);
-    matrix2.set(0, 0, 1);
-    matrix2.set(2, 2, 4);
-    matrix2.set(2, 0, 5);
-    matrix2.set(0, 2, 2);
-    matrix2.print_readable();
-
-    std::cout << '\n' << "Product of two matrices: " << '\n';
-    nn::Matrix prod_matrix = matrix1.dot_matrix(matrix2);
-    prod_matrix.print_readable();
-
     //nn::Matrix wrong_prod = matrix1.dot_matrix(matrix3);
 
-    nn::Neuron neuron = nn::Neuron(5);
-    neuron.print_weights();
+    //nn::Neuron neuron = nn::Neuron(5);
+    //neuron.print_weights();
 
-    nn::MLP mlp = nn::MLP(7, 4, 4, 7, 9);
+    //nn::MLP mlp = nn::MLP(7, 4, 4, 7, 9);
 
-    size_t j = 0;
-    for (nn::Layer& layer : mlp.get_layers()) {
-        if (j == mlp.num_layers() - 1) {
-            layer.set_activation_function(nn::sigmoid);
-        } else {
-            layer.set_activation_function(nn::relu);
-        }
-        ++j;
+    //size_t j = 0;
+    //for (nn::Layer& layer : mlp.get_layers()) {
+    //    if (j == mlp.num_layers() - 1) {
+    //        layer.set_activation_function(nn::sigmoid, nn::sigmoid_prime);
+    //    } else {
+    //        layer.set_activation_function(nn::relu, nn::relu_prime);
+    //    }
+    //    ++j;
+    //}
+
+
+    //std::vector<float> input = {1, 2, 255, 255, 0, 0, 6};
+    //mlp.feed_input(input);
+
+    //std::cout << "Inputs fed into the network: " << std::endl;
+
+    //std::vector<float> outputs = mlp.get_layers()[0].get_outputs();
+    //for (float output : outputs) {
+    //    std::cout << output << ", ";
+    //}
+
+    //std::cout << std::endl;
+
+    //mlp.feed_forward(); // so far no activations are implemented
+
+    //std::cout << "Outputs of the network: " << std::endl;
+
+    //std::vector<float> out = mlp.predict();
+    //for (float output : out) {
+    //    std::cout << output << ", ";
+    //}
+
+    //std::cout << std::endl;
+
+    //std::cout << "Current network configuration: " << std::endl;
+    //size_t i = 0;
+    //for (nn::Layer layer : mlp.get_layers()) {
+    //    std::cout << "Layer: " << i << std::endl;
+    //    for (nn::Neuron neuron : layer.get_neurons()) {
+    //        neuron.print_weights();
+    //        std::cout << "Output: " << neuron.output() << std::endl;
+    //    }
+
+    //    std::cout << '\n';
+    //    ++i;
+    //}
+
+    std::cout << "BACKPROPAGATION TEST" << std::endl;
+    nn::MLP mlp1(3, 4, 2);
+
+    mlp1.get_layers()[1].set_activation_function(nn::relu, nn::relu_prime);     // Hidden layer
+    mlp1.get_layers()[2].set_activation_function(nn::sigmoid, nn::sigmoid_prime); // Output layer
+
+    // Sample input and target
+    std::vector<float> input1 = {0.5, 0.1, -0.3};
+    std::vector<float> target1 = {1.0, 0.0};  // Example target
+
+    for (int epoch = 0; epoch < 1000; ++epoch) {
+        mlp1.feed_input(input1);
+        mlp1.feed_forward();
+        mlp1.backward_prop(target1, 0.01);
     }
 
 
-    std::vector<float> input = {1, 2, 255, 255, 0, 0, 6};
-    mlp.feed_input(input);
-
-    std::cout << "Inputs fed into the network: " << std::endl;
-
-    std::vector<float> outputs = mlp.get_layers()[0].get_outputs();
-    for (float output : outputs) {
-        std::cout << output << ", ";
+    std::vector<float> output1 = mlp1.predict();
+    std::cout << "Output after training: ";
+    for (float val : output1) {
+        std::cout << val << " ";
     }
-
     std::cout << std::endl;
-
-    mlp.feed_forward(); // so far no activations are implemented
-
-    std::cout << "Outputs of the network: " << std::endl;
-
-    std::vector<float> out = mlp.predict();
-    for (float output : out) {
-        std::cout << output << ", ";
-    }
-
-    std::cout << std::endl;
-
-    std::cout << "Current network configuration: " << std::endl;
-    size_t i = 0;
-    for (nn::Layer layer : mlp.get_layers()) {
-        std::cout << "Layer: " << i << std::endl;
-        for (nn::Neuron neuron : layer.get_neurons()) {
-            neuron.print_weights();
-            std::cout << "Output: " << neuron.output() << std::endl;
-        }
-
-        std::cout << '\n';
-        ++i;
-    }
 }
