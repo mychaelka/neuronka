c8809e18587e734ea59e21228d443a5cc885b09c
Michaela <mychaela.k98@gmail.com>
2024-11-13 14:05:08 +0100

    Throw out Neuron class

--- src/nn.hpp
+++ src/nn.hpp
@@ -5,103 +5,61 @@
 
 #pragma once
 
-// class neuron: inputs, weights, bias, activation
-// scaling the data? 
-// TODO: start with individual examples, then extend to batch processing
+// TODO: scale the input data
 
 namespace nn {
-
-    class Neuron {
-    private:
-        Vector _weights;
-        size_t _n_weights;
-        float _bias;
-        float _output;
-        float _delta;
-    
-    public:
-        Neuron(size_t n_weights) : _n_weights(n_weights), _weights(n_weights), _delta(0.0f) {
-            init_weights();
-        }
-
-        size_t n_weights() const { return _n_weights; }
-        
-        Vector& weights() { return _weights; }
-        const Vector& get_weights() const { return _weights; }
-        
-        void set_weights(std::vector<float>& new_weights) { 
-            _weights.set_weights(new_weights);
-        }
-
-        float output() const { return _output; }
-
-        void set_output(float output) { _output = output; }
-
-        float delta() const { return _delta; }
-
-        void set_delta(float delta) { _delta = delta; }
-
-        float bias() const { return _bias; }
-
-        void set_bias(float bias) { _bias = bias; }
-
-        void set_inner_potential(const nn::Vector& inputs) {
-            _output = _weights.dot_product(inputs) + _bias;
-        }
-
-        void print_weights() {
-
-            std::cout << "Weights: ";
-
-            for (size_t i = 0; i < _n_weights; ++i) {
-                std::cout << _weights[i] << ", ";
-            }
-
-            std::cout << std::endl;
-            std::cout << "Bias: " << _bias << std::endl;
-        }
-
-        void init_weights() {
-            std::random_device rd;
-            std::mt19937 generator(rd());
-
-            std::normal_distribution<float> distribution(0.0f, std::sqrt(2.0f / _n_weights));
-            std::uniform_real_distribution<float> bias_distribution(-0.1f, 0.1f);
-
-            for (size_t i = 0; i < _n_weights; ++i) {
-                _weights[i] = distribution(generator);
-            }
-
-            _bias = bias_distribution(generator);
-        }
-    };
-
-
     class Layer {
+    
     private:
-        std::vector<Neuron> _neurons;
+    
+        size_t _output_size;
+        size_t _input_size;
+        
+        const Matrix* _input;
+        Matrix _output;
+        Matrix _weights;
+        Matrix _biases;
+
+        Matrix _weights_delta;
+        Matrix _bias_delta;
+
         std::function<float(float)> _activation_function;
         std::function<float(float)> _activation_derivative;
     
     public:
-        Layer(size_t n_neurons, size_t n_weights_per_neuron)
-         : _activation_function(identity), _activation_derivative(identity_prime) {
-            for (size_t i = 0; i < n_neurons; ++i) {
-                _neurons.emplace_back(n_weights_per_neuron);
-            }
+        
+        Layer(size_t output_size, size_t input_size)
+         : _output_size(output_size),
+         _input_size(input_size),
+         _input(nullptr),
+         _output(output_size, 1),
+         _weights(output_size, input_size),
+         _biases(output_size, 1),
+         _weights_delta(output_size, input_size),
+         _bias_delta(output_size, 1),
+         _activation_function(identity), 
+         _activation_derivative(identity_prime) {
         }
 
-        std::vector<float> get_outputs() const {
-            std::vector<float> outputs;
-            for (const Neuron& neuron : _neurons) {
-            outputs.push_back(neuron.output());
-            }
-
-            return outputs;
+        const Matrix& get_output() const {
+            return _output;
         }
 
-        const std::vector<Neuron>& get_neurons() const { return _neurons; }
-        std::vector<Neuron>& get_neurons() { return _neurons; }
+        void init_weights(float min_value = 0.0f, float max_value = 1.0f) {
+            std::random_device rd;
+            std::mt19937 generator(rd());
+
+            //std::normal_distribution<float> distribution(0.0f, std::sqrt(2.0f / _input_size));
+            std::uniform_real_distribution<float> distribution(min_value, max_value);
+
+            for (float& weight : _weights.elements()) {
+                weight = distribution(generator);
+            }
+
+            for (float& bias : _biases.elements()) {
+                bias = distribution(generator);
+            }
+        }
 
         void set_activation_function(std::function<float(float)> activation_function, 
                                     std::function<float(float)> activation_derivative) {
@@ -109,80 +67,63 @@ namespace nn {
             _activation_derivative = activation_derivative;
         }
 
-        void compute_output_deltas(const std::vector<float>& target) {
-            for (size_t i = 0; i < _neurons.size(); ++i) {
-                float output = _neurons[i].output();
-                float error = target[i] - output;
-                _neurons[i].set_delta(error * _activation_derivative(output));
+        void output() {
+            if (_input == nullptr) {
+                throw std::runtime_error("Input not set for layer");
             }
+            
+            _output = _weights.dot_matrix(*_input); // creates new matrix
+            _output += _biases; // modifies _output directly
+            _output.map(_activation_function); // modifies _output directly
         }
 
-        void compute_hidden_deltas(const Layer& next_layer) {
-            for (size_t i = 0; i < _neurons.size(); ++i) {
-                float sum = 0.0f;
-                
-                for (const Neuron& next_neuron : next_layer.get_neurons()) {
-                    sum += next_neuron.get_weights()[i] * next_neuron.delta();
-                }
-                
-                _neurons[i].set_delta(sum * _activation_derivative(_neurons[i].output()));
-            }
-        }
-
-        void update_weights(const std::vector<float>& prev_outputs, float learning_rate) {
-            for (Neuron& neuron : _neurons) {
-                for (size_t j = 0; j < neuron.n_weights(); ++j) {
-                    neuron.weights()[j] += learning_rate * neuron.delta() * prev_outputs[j];
-                }
-                neuron.set_bias(neuron.bias() + learning_rate * neuron.delta());
-            }
-        }
-
-
-        void activate() {
-            for (Neuron& neuron : _neurons) {
-                float activated_output = _activation_function(neuron.output());
-                neuron.set_output(activated_output);
-            }
+        void input(const Matrix& input) {
+            _input = &input;
         }
     };
 
 
     class MLP {
-    private:
-        std::vector<Layer> _layers;
-        size_t _n_layers;
-
-        template<typename... Sizes>
-        void create_layers(size_t input_size, size_t hidden_size, Sizes... sizes) {
-            _layers.emplace_back(hidden_size, input_size);
-            create_layers(hidden_size, sizes...);
-        }
-
-        void create_layers(size_t hidden_size, size_t output_size) {
-            _layers.emplace_back(output_size, hidden_size);
-        }
-
     
-    public:
-        template<typename... Sizes>
-        MLP(Sizes... sizes) {
-            static_assert(sizeof...(sizes) >= 2, "At least two sizes (input and output layers) are required.");
-            
-            auto get_first = [](auto first, auto... rest) { return first; };
+    private:
 
-            size_t input_size = get_first(sizes...);
-            _layers.emplace_back(input_size, 0);
-            create_layers(sizes...);
+        std::vector<size_t> _neurons;
+        std::vector<Layer> _layers;
+ 
+    public:
+
+        MLP(std::initializer_list<size_t> layer_sizes) {
+            
+            auto it = layer_sizes.begin();
+            size_t input_size = *it++;
+
+            for (; it != layer_sizes.end(); ++it) {
+                size_t output_size = *it;
+                Layer layer(output_size, input_size);
+
+                layer.init_weights();
+                _layers.push_back(layer);
+
+                input_size = output_size;
+            }
         }
 
         const std::vector<Layer>& get_layers() const { return _layers; }
         std::vector<Layer>& get_layers() { return _layers; }
 
-        size_t num_layers() { return _n_layers; }
+        /* Matrix feed_forward(const Matrix& input) {
+            Matrix current_output = input;
+            
+            for (Layer& layer : _layers) {
+                layer.set_input(current_output);
+                layer.output();
+                current_output = layer.get_output();
+            }
 
-
-        void feed_input(const std::vector<float>& input_vector) {
+            return current_output;
+        }
+ */
+        /* void feed_input(const std::vector<float>& input_vector) {
             size_t i = 0;
             
             for (Neuron& input_neuron : _layers[0].get_neurons()) { // for every neuron in input layer...
@@ -221,7 +162,7 @@ namespace nn {
 
         float compute_loss(const std::vector<float>& target) const {
             return cross_entropy_softmax(_layers.back().get_outputs(), target);
-        }
+        } */
         
         float accuracy();
     };
@@ -230,7 +171,7 @@ namespace nn {
 
 void train(nn::MLP& network, const std::vector<std::vector<float>>& inputs, const std::vector<std::vector<float>>& targets, int epochs, float learning_rate) {
     
-    for (int epoch = 0; epoch < epochs; ++epoch) {
+    /* for (int epoch = 0; epoch < epochs; ++epoch) {
         float total_loss = 0.0f;
 
         for (size_t i = 0; i < inputs.size(); ++i) {
@@ -253,5 +194,5 @@ void train(nn::MLP& network, const std::vector<std::vector<float>>& inputs, cons
         }
 
         std::cout << "Epoch " << epoch + 1 << "/" << epochs << ", Loss: " << total_loss / inputs.size() << std::endl;
-    }
+    } */
 }
