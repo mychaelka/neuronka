013fdc9baa1fb5ac02217d080c00c32daa4cbd2a
Michaela <mychaela.k98@gmail.com>
2024-11-18 16:53:33 +0100

    Remove XOR and finally test on MNIST

--- src/tests.hpp
+++ src/tests.hpp
@@ -24,74 +24,44 @@ void write_predictions(const std::string& filename, const std::vector<float>& pr
 
 void tests() {
 
-    // Bad input file, should fail
-    std::vector<std::vector<float>> bad_input = parse_input("nonexistent_file.csv");
-    std::vector<std::vector<float>> good_input = parse_input("./data/fashion_mnist_train_vectors.csv");
-
-    nn::Matrix matrix = nn::Matrix(5, 1);
-    matrix.set(0, 0, 1);
-    matrix.set(1, 0, -1);
-    matrix.set(2, 0, 5);
-    matrix.set(4, 0, -7);
-    matrix.print_readable();
-    //matrix.map([](float x) { return x * x; });
-    matrix.map(nn::relu);
-    //matrix.print_readable();
-
     // MNIST
     std::vector<std::vector<float>> train_inputs = parse_input("./data/fashion_mnist_train_vectors.csv");
-    std::vector<float> num_labels = parse_labels("./data/fashion_mnist_train_labels.csv");
-    std::vector<std::vector<float>> train_labels = nn::one_hot_all_labels(num_labels);
+    std::vector<std::vector<float>> test_inputs = parse_input("./data/fashion_mnist_test_vectors.csv");
 
-    nn::Matrix label_matrix = nn::Matrix(train_labels).transpose();
-    nn::Matrix input_matrix = nn::Matrix(train_inputs).transpose();
-    std::cout << input_matrix.nrows() << " ";
-    std::cout << input_matrix.ncols() << std::endl;
+    std::vector<float> train_labels = parse_labels("./data/fashion_mnist_train_labels.csv");
+    std::vector<std::vector<float>> train_targets = nn::one_hot_all_labels(train_labels);
 
-    std::cout << label_matrix.nrows() << " ";
-    std::cout << label_matrix.ncols() << std::endl;
+    std::vector<float> test_labels = parse_labels("./data/fashion_mnist_test_labels.csv");
+    std::vector<std::vector<float>> test_targets = nn::one_hot_all_labels(test_labels);
 
-    //mlp.feed_forward(input_matrix);
-    //nn::Matrix out = mlp.layers().back().get_output();
+    nn::Matrix train_input_matrix = nn::Matrix(train_inputs).transpose();
+    nn::Matrix train_target_matrix = nn::Matrix(train_targets).transpose();
+
+    nn::Matrix test_input_matrix = nn::Matrix(test_inputs).transpose();
+    nn::Matrix test_target_matrix = nn::Matrix(test_targets).transpose();
     
+    size_t train_size = 60000;
+    size_t test_size = 10000;
+    size_t input_size = 784;
+    size_t num_classes = 10;
+    size_t batch_size = 128;
 
-    /* train(mlp, inputs, labels, epochs, learning_rate);
+    nn::MLP mnist_network({input_size, 32, 64, num_classes}, batch_size);
 
-    std::cout << "Predictions after training:" << std::endl;
-    for (const auto& input : inputs) {
-        mlp.feed_input(input);
-        mlp.feed_forward();
-        std::vector<float> output = mlp.predict();
+    train_input_matrix.normalize();
+    test_input_matrix.normalize();
 
-        for (float val : output) {
-            std::cout << val << " ";
-        }
-        std::cout << std::endl;
-    } */
+    int epochs = 100;
+    float learning_rate = 0.01f;
 
-   /* XOR */
-    nn::Matrix xor_input_matrix(2, 4, {0, 0, 1, 1, 0, 1, 1, 0});
-    nn::Matrix xor_target_matrix(2, 4, {1, 0, 1, 0, 0, 1, 0, 1});
+    auto input_batches = nn::create_batches(train_input_matrix, batch_size);
+    auto target_batches = nn::create_batches(train_target_matrix, batch_size);
 
-    nn::MLP xor_network({2, 4, 2}, 4); // 2 input neurons, 2 hidden neurons, 1 output neurons, batch size = 4
+    nn::train(mnist_network, input_batches, target_batches, epochs, learning_rate);
 
-    // Train the network
-    int epochs = 1000;
-    float learning_rate = 0.05f;
+    std::vector<float> train_predictions = mnist_network.predict(train_input_matrix);
+    std::vector<float> test_predictions = mnist_network.predict(test_input_matrix);
 
-    for (int epoch = 0; epoch < epochs; ++epoch) {
-        const nn::Matrix& outputs = xor_network.feed_forward(xor_input_matrix);
-        
-        float loss = nn::cross_entropy_loss(outputs, xor_target_matrix);
-        
-        xor_network.backward(xor_target_matrix, learning_rate);
-
-        if (epoch % 100 == 0) {
-            std::cout << "Epoch " << epoch << ", Loss: " << loss << std::endl;
-        }
-    }
-
-    std::vector<float> predictions = xor_network.predict(xor_input_matrix);
-    write_predictions("output.csv", predictions);
-
+    write_predictions("train_predictions.csv", train_predictions);
+    write_predictions("test_predictions.csv", test_predictions);
 }
